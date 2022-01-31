ac68acb6b840ef2c7f540acf1584058492b322bb
Bsrirangapurapu <96447436+Bsrirangapurapu@users.noreply.github.com>
2022-01-31 12:30:36 -0500

    Add files via upload

---
+++ Day2_B57.py
@@ -0,0 +1,165 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+understanding variable in python
+
+
+# In[ ]:
+
+
+
+
+
+# In[2]:
+
+
+x= 5
+y =2
+
+
+# In[3]:
+
+
+x+y
+
+
+# In[ ]:
+
+
+rules for declaring variables
+
+
+# In[ ]:
+
+
+1. No namespaces whicle declaring the variable name
+
+
+# In[5]:
+
+
+firstname = 'Bhargavi'
+print(firstname)
+
+
+# In[6]:
+
+
+first_name = 'Bhargavi'
+print(first_name)
+
+
+# In[ ]:
+
+
+**2. A variable name should not start with a number.
+
+
+# In[7]:
+
+
+1name = 'Bhargavi'
+print(1name)
+
+
+# In[8]:
+
+
+name1 = 'Bhargavi'
+print(name1)
+
+
+# In[ ]:
+
+
+3. A variable name should not contain any special characters @$%&
+
+
+# In[9]:
+
+
+name@ = 'Bhargavi'
+print(name@)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+introduction to datatypes in python:
+    1. string
+    2. list
+    3. numbers --> integers and floats (decimals)
+    4. tuple
+    4. dictionaries
+
+
+# In[ ]:
+
+
+classification of datatyoes:
+    two different categories:
+        1. mutable datatypes --> which we can edit and alter --> flexible in nature
+        2. immutable datatype --> which we cannot edit or alter ---> fixed in nature
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+Introduction to string datatype
+defination: A string is nothing but a series of characters declared in quotes
+classification: it is classified as immutable datatype (fixed in nature)
+    
+how to decalre a string:    
+    3 possibles approaches
+        1. single quote
+        2. double quote
+        3. triple quote
+
+
+# In[10]:
+
+
+name = 'Bhargavi'
+print(name)
+
+
+# In[11]:
+
+
+city = "Richmond"
+print(city)
+
+
+# In[13]:
+
+
+color = """Yellow"""
+print(color)
+
+
+# In[ ]:
+
+
+# is used for commenting the lines..
+
+
+# In[ ]:
+
+
+
+
