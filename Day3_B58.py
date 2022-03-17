90ce49fcc081fd4849b2aad707be305bbe6e626d
Bsrirangapurapu <96447436+Bsrirangapurapu@users.noreply.github.com>
2022-03-17 15:56:10 -0400

    Add files via upload

---
+++ Day3_B58.py
@@ -0,0 +1,177 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# Thursday, March 17, 2022
+
+
+# In[ ]:
+
+
+#Continuation with strings
+
+
+# In[ ]:
+
+
+#Introduction to f strings
+#general syntax of f strings
+
+
+# In[ ]:
+
+
+f"custom words {Placeholder1} {placeholder2}......{placeholder...n}"
+
+
+# In[8]:
+
+
+firstname = 'bhargavi'
+lastname = 'srirangapurapu'
+
+
+# In[ ]:
+
+
+#req: i want to get the full name 
+
+
+# In[9]:
+
+
+fullname = f"{firstname} {lastname}"
+
+print(fullname)
+
+
+# In[10]:
+
+
+print(fullname.title())
+
+
+# In[11]:
+
+
+print(f"Keep up the good work, {fullname.title()}")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+#adding white spaces to strings
+
+
+# In[12]:
+
+
+print("Favourite_Language:Phythonc++javacswiftsql")
+
+
+# In[16]:
+
+
+print("Favourite_Language:\nPhython\nc++\njava\nc\nswift\nsql")
+
+
+# In[ ]:
+
+
+#\n new line delimiter
+
+
+# In[15]:
+
+
+print("Favourite_Language:\n\tPhython\n\tc++\n\tjava\n\tc\n\tswift\n\tsql")
+
+
+# In[ ]:
+
+
+#\t tab delimiter
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+how to remove white spaces from the strings:
+
+
+# In[17]:
+
+
+name = 'python'
+print(name)
+
+
+# In[19]:
+
+
+name2 = '  java'
+print(name2)
+
+
+# In[20]:
+
+
+name3 = 'sql   '
+print(name3)
+
+
+# In[ ]:
+
+
+
+
+
+# In[22]:
+
+
+name2.lstrip()
+
+
+# In[23]:
+
+
+name3.rstrip()
+
+
+# In[24]:
+
+
+name2.strip()
+
+
+# In[25]:
+
+
+name3.strip()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
